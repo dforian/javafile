872faa4f4fbc202df4ed5199ca3510f7409f4b42
dforian <68153509+dforian@users.noreply.github.com>
2021-04-29 13:37:55 +0300

    File project

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,24 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) throws IllegalArgumentException {
+
+        if (contacts == null || output == null) throw new IllegalArgumentException();
+
+        try {
+            PrintWriter writer = new PrintWriter(new File(output));
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                writer.println(entry.getKey() + ": " + entry.getValue());
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
